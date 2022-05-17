f4cc29cb851145b971b6634828ce2f5315451559
Suzan-Queiroz <Suzanliviaqueiroz@icloud.com>
2022-05-17 11:21:11 -0300

    Adição da tabala Lab e seus comandos

--- LabManager/Program.cs
+++ LabManager/Program.cs
@@ -14,6 +14,11 @@ command.CommandText = @"
         ram varchar(100) not null,
         processor varchar(100) not null
     );
+    CREATE TABLE IF NOT EXISTS Lab(
+        id int not null primary key,
+        number int not null,
+        name varchar(100) not null,
+        block varchar(50) not null
 )";
 
 //Execute
@@ -64,4 +69,49 @@ if(modelName == "Computer")
         command.ExecuteNonQuery();
         connection.Close();
     }
+}
+
+if (modelName == "Lab")
+{
+    if (modelAction == "List")
+        {
+        Console.WriteLine("List Lab");
+        connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+
+        command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Lab;";
+        
+        var reader = command.ExecuteReader();
+        
+        while (reader.Read())
+            {
+            Console.WriteLine(
+                "{0}, {1}, {2}, {3}", reader.GetInt32(0), reader.GetInt32(1), 
+                reader.GetString(2), reader.GetString(3)
+                );
+            }
+        connection.Close();
+        }
+
+        if (modelAction == "New")
+        {
+        int id = Convert.ToInt32(args[2]);
+        int number = Convert.ToInt32(args[3]);
+        string name = args[4];
+        string block = args[5];
+
+        connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+
+        command = connection.CreateCommand();
+        command.CommandText = "INSERT INTO Lab VALUES($id, $number, $name, $block);";
+        command.Parameters.AddWithValue("$id", id);
+        command.Parameters.AddWithValue("$number", number);
+        command.Parameters.AddWithValue("$name", name);
+        command.Parameters.AddWithValue("$block", block);
+
+        command.ExecuteNonQuery();
+        connection.Close();
+        }
 }
